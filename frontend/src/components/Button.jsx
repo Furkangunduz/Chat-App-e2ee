a44a423e0b78581b34dee79aae01fecb2c27f113
Furkan <furkan.gundz00@gmail.com>
2022-09-26 11:14:50 +0300

    Btn Hover.

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button({ children }) {
 	return (
-		<button className=' px-2 py-2 bg-dark-blue text-white font-semibold border-2 border-black/20 rounded-lg hover:scale-[1.05] hover:bg-dark-blue/95 transition-transform'>
+		<button className=' px-2 py-2 bg-dark-blue text-white font-semibold border-2 border-black/20 rounded-lg hover:scale-[1.05] hover:bg-dark-blue/95 hover:-translate-y-1 transition-transform'>
 			{children}
 		</button>
 	);
