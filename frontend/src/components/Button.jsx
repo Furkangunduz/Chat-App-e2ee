f8256f898d065bff1ee0f402f04d605abbac0e4d
Furkan <furkan.gundz00@gmail.com>
2022-09-26 10:26:26 +0300

    Create seperate Button component.

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button({ children }) {
+	return (
+		<button className=' px-2 py-2 bg-dark-blue text-white font-semibold border-2 border-black/20 rounded-lg hover:scale-[1.05] hover:bg-dark-blue/95 transition-transform'>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
