cb8f68c2321475df12dab4c4b76cc51b6d925016
Furkan <furkan.gundz00@gmail.com>
2022-09-30 12:02:45 +0300

    Scroll Messages container every message receive.

--- frontend/src/components/Home/ChatMessages.jsx
+++ frontend/src/components/Home/ChatMessages.jsx
@@ -1,38 +1,20 @@
 import ChatMessagesItem from './ChatMessagesItem';
+import { useContext, useRef, useEffect } from 'react';
+import ChatContext from '../../context/ChatContext';
 
 function ChatMessages() {
+	const { chatHistory } = useContext(ChatContext);
+	const messagesRef = useRef(null);
+
+	useEffect(() => {
+		messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+	}, [chatHistory]);
+
 	return (
-		<div className='max-h-[70%] w-[90%] ml-10 pt-5 overflow-y-auto overflow-x-hidden backdrop-blur-xl'>
-			{[
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'me' },
-				{
-					text: 'Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello world',
-					sender: 'friend',
-				},
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'me' },
-				{ text: 'Hello world', sender: 'me' },
-				{
-					text: 'Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world ',
-					sender: 'me',
-				},
-				{
-					text: 'Hello world Hello world Hello world Hello world Hello world Hello world Hello world ',
-					sender: 'friend',
-				},
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'me' },
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'me' },
-				{ text: 'Hello world', sender: 'me' },
-				{ text: 'Hello world', sender: 'me' },
-				{ text: 'Hello world', sender: 'friend' },
-				{ text: 'Hello world', sender: 'friend' },
-			].map((message, indx) => (
+		<div
+			ref={messagesRef}
+			className='max-h-[70%] w-[90%] ml-10 pt-5 overflow-y-auto overflow-x-hidden backdrop-blur-xl'>
+			{chatHistory.map((message, indx) => (
 				<ChatMessagesItem key={indx} message={message} />
 			))}
 		</div>
