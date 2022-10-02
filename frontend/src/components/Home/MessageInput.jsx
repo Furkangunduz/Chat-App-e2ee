8cb21285d1d84d5b467ce2c584ab630241bd96af
Furkan <furkan.gundz00@gmail.com>
2022-10-02 13:36:18 +0300

    Refactor send message function.

--- frontend/src/components/Home/MessageInput.jsx
+++ frontend/src/components/Home/MessageInput.jsx
@@ -1,7 +1,10 @@
 import { useState, useContext } from 'react';
 import { FiSend } from 'react-icons/fi';
 import { toast } from 'react-toastify';
+
 import RSA from '../../utils/keygenerator';
+import { divideStringByLength } from '../../utils/utils';
+
 import ChatContext from '../../context/ChatContext';
 import SocketContext from '../../context/SocketContext';
 
@@ -11,37 +14,26 @@ function MeesageInput() {
 	const { socket } = useContext(SocketContext);
 
 	const sendMessage = () => {
-		if (activeChatPublicKey) {
-			let msg = message;
-			msg = msg.replaceAll('ş', 's');
-			if (msg.trim().length == 0) {
-				return;
-			} else if (msg.trim().length < 20) {
-				socket.emit('newMessage', RSA.encrypt(RSA.encode(msg), activeChatPublicKey));
-				setChatHistory((prev) => [...prev, { sender: 'me', text: msg }]);
-				setMessage('');
-			} else {
-				let messagesArray = [];
-				let encryptedMessagesArray = [];
-				while (true) {
-					if (msg.length <= 17) {
-						messagesArray.push(msg.substring(0, msg.length));
-						break;
-					}
-					messagesArray.push(msg.substring(0, 17));
-					msg = msg.substring(17);
-				}
-				messagesArray.forEach((message) => {
-					encryptedMessagesArray.push(
-						RSA.encrypt(RSA.encode(message), activeChatPublicKey)
-					);
-				});
-				socket.emit('newMessage', encryptedMessagesArray);
-			}
-		} else {
-			toast.error('Cant send messaages.');
+		if (!activeChatPublicKey) return;
+		if (!message || message.trim().length === 0) return;
+		let msg = message.replaceAll('ş', 's').trim();
+
+		setChatHistory((prev) => [...prev, { sender: 'me', text: msg }]);
+
+		if (msg.length < 20) {
+			socket.emit('newMessage', RSA.encrypt(msg, activeChatPublicKey));
+			return;
 		}
+
+		let messagesArray = divideStringByLength(msg, 17);
+		let encryptedMessagesArray = [];
+		messagesArray.forEach((message) => {
+			encryptedMessagesArray.push(RSA.encrypt(message, activeChatPublicKey));
+		});
+		socket.emit('newMessage', encryptedMessagesArray);
+		setMessage('');
 	};
+
 	return (
 		<div className='w-full absolute bottom-0 flex justify-center items-center gap-6 mb-4 pt-4 border-t-2 border-black/10'>
 			<input
